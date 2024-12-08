2ca45b5a21e6bfc173deb35fd020325a583f29b6
Apolos666 <quangprox121@gmail.com>
2024-12-08 22:06:49 +0700

    Bai 217 Complete

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int[] nums = [1,2,3,1];
+
+var result = ContainsDuplicate(nums);
+
+Console.WriteLine(result);
+
+bool ContainsDuplicate(int[] nums) {
+    HashSet<int> hashset = [];
+
+    foreach (var num in nums) {
+        if (hashset.TryGetValue(num, out var actualNum)) 
+            return true;
+        hashset.Add(num);
+    }
+
+    return false;
+}
