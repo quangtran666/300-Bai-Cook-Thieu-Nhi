9749ac59bfd1bcfe137079e19cd583c5b16af5d0
Apolos666 <quangprox121@gmail.com>
2024-12-09 08:29:02 +0700

    my-version-sort-hashtable: Bai 49

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿string[] strs = ["eat","tea","tan","ate","nat","bat"];
+
+var result = GroupAnagrams(strs);
+
+result.ToList().ForEach(x => {
+    Console.WriteLine(x);
+});
+
+IList<IList<string>> GroupAnagrams(string[] strs) {
+    var dict = new Dictionary<string, IList<int>>();
+    var result = new List<IList<string>>();
+
+    for (int i = 0; i < strs.Length; i++) {
+        var characters = strs[i].ToCharArray();
+        Array.Sort(characters);
+        if (dict.TryGetValue(new string(characters), out var actualValue)) {
+            actualValue.Add(i);
+            continue;
+        }
+        dict[new string(characters)] = [i];
+    }
+
+    foreach (var key in dict.Keys) {
+        var anagramTogether = dict[key].Select(x => strs[x]).ToList();
+        result.Add(anagramTogether);
+    }
+
+    return result;
+}
