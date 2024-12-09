d9c6e0bcff25389ade438f5bda3ce0bc223cb48c
Apolos666 <quangprox121@gmail.com>
2024-12-09 08:00:37 +0700

    Bai 1 Complete

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿int[] nums = [3, 3];
+
+var result = TwoSum(nums, 6);
+
+Console.WriteLine(result[0] + " " + result[1]);
+
+int[] TwoSum(int[] nums, int target) {
+    var dict = new Dictionary<int, int>();
+    int[] result = [];
+
+    for (var i = 0; i < nums.Length; i++) {
+        if (dict.TryGetValue(target - nums[i], out var actualValue)) {
+            result = [actualValue, i];
+        }
+
+        dict.TryAdd(nums[i], i);
+    }
+
+    return result;
+}
