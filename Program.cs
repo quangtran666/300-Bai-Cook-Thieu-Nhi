5d01ff17622dc7b1b91ab806f4d996d0b3d03ade
Apolos666 <quangprox121@gmail.com>
2024-12-09 23:01:35 +0700

    Bai 347 Complete

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿int[] nums = [1,1,1,2,2,3];
+
+var result = TopKFrequent(nums, 2);
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+
+int[] TopKFrequent(int[] nums, int k) {
+    var dict = new Dictionary<int, int>();   
+
+    foreach (var num in nums) {
+        dict[num] = dict.GetValueOrDefault(num, 0) + 1;
+    }
+
+    return dict
+            .OrderByDescending(x => x.Value)
+            .Select(x => x.Key)
+            .Take(k)
+            .ToArray();
+}   
