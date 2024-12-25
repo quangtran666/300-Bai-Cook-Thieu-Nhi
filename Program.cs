278789262181398dd2f6ada8e2d4eb13c05a744c
Apolos666 <quangprox121@gmail.com>
2024-12-25 13:50:52 +0700

    Bai 125 My Version

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System.Text.RegularExpressions;
+
+var s = "race a car";
+
+var result = IsPalindrome(s);
+Console.WriteLine(result);
+
+bool IsPalindrome(string s)
+{
+    var cleanString = CleanString(s);
+
+    for (var i = 0; i < cleanString.Length; i++)
+    {
+        if (cleanString[i] == cleanString[cleanString.Length - 1 - i])
+            continue;
+        return false;
+    }
+
+    return true;
+}
+
+string CleanString(string s)
+{
+    return Regex.Replace(s.ToLower(), "[^a-zA-Z0-9]", "");
+}
