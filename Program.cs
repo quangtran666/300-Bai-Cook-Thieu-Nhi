7e800e25271e9e61a5d151c75807f4c4d9fd6fa7
Apolos666 <quangprox121@gmail.com>
2024-12-30 15:47:00 +0700

    Bai 11 My Solution and Neetcode

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿var height = new int[] {1,8,6,2,5,4,8,3,7};
+
+var result = MaxArea(height);
+Console.WriteLine(result);
+
+int MaxArea(int[] height)
+{
+    var maxWater = 0;
+    int left = 0, right = height.Length - 1;
+
+    while (left < right)
+    {
+        var area = Math.Min(height[left], height[right]) * (right - left);
+        maxWater = Math.Max(area, maxWater);
+
+        if (height[left] <= height[right])
+            left++;
+        else
+            right--;
+    }
+    
+    return maxWater;
+}
