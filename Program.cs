28f6106ad1c71b12350d7991a84f338c3d5bd939
Apolos666 <quangprox121@gmail.com>
2024-12-30 13:47:14 +0700

    Bai 167 My Version

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿var numbers = new int[] { 2, 7, 11, 15 };
+var target = 9;
+
+var result = TwoSum(numbers, target);
+foreach (var i in result)
+{
+   Console.WriteLine(i);
+}
+
+int[] TwoSum(int[] numbers, int target)
+{
+   int left = 0, right = numbers.Length - 1;
+
+   while (left != right)
+   {
+      if (numbers[left] + numbers[right] == target)
+         return [left + 1, right + 1];
+      
+      if (numbers[left] + numbers[right] < target)
+         left++;
+      else if (numbers[left] + numbers[right] > target)
+         right--;
+   }
+
+   return [];
+}
