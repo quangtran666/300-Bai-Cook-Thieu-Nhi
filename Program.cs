a8cf6ea155d3eeafa9126e384b3ac60e999d0b7a
Apolos666 <quangprox121@gmail.com>
2024-12-31 15:00:28 +0700

    Bai 121 My solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿var prices = new int[] {7,1,5,3,6,4};
+
+Console.WriteLine(MaxProfit(prices));
+
+int MaxProfit(int[] prices)
+{
+    int start = 0, end = 0, max = 0;
+    
+    for (var i = 1; i < prices.Length; i++)
+    {
+        if (prices[i] < prices[start])
+        {
+            start = i;
+            end = i;
+        }
+
+        end = i;
+        
+        max = Math.Max(max, prices[end] - prices[start]);
+    }
+
+    return max;
+}
