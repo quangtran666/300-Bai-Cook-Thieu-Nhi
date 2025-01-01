6bd60c0e2196e27ed1d236a794cca4cf9456ed10
Apolos666 <quangprox121@gmail.com>
2025-01-01 14:42:03 +0700

    Bai 3 My solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿var s = "dvdf";
+
+var result = LengthOfLongestSubstring(s);
+
+Console.WriteLine(result);
+
+int LengthOfLongestSubstring(string s)
+{
+    var hashset = new HashSet<char>();
+    var start = 0;
+    var end = 0;
+    var max = 0;
+
+    for (end = 0; end < s.Length; end++)
+    {
+        while (hashset.Contains(s[end]) && start < end)
+        {
+            hashset.Remove(s[start]);
+            start++;
+        }
+
+        hashset.Add(s[end]);
+
+        max = Math.Max(max, end - start + 1);
+    }
+
+    return max;
+}
