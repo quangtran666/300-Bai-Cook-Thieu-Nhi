de9d6ccd5f7dd9231baa87067b02447c5c7adff3
Apolos666 <quangprox121@gmail.com>
2025-01-04 16:11:22 +0700

    Bai 20 My solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿var s = "()[]{}";
+
+var result = IsValid(s);
+Console.WriteLine(result);
+
+bool IsValid(string s) {
+    var stack = new Stack<char>();
+    var dictMap = new Dictionary<char, char>()
+    {
+        ['('] = ')',
+        ['{'] = '}',
+        ['['] = ']'
+    };
+    
+    foreach (var @char in s)
+    {
+        if (dictMap.ContainsKey(@char))
+            stack.Push(@char);
+        else if (stack.Count > 0 && dictMap[stack.Peek()] == @char)
+            stack.Pop();
+        else
+            return false;
+    }
+    
+    return stack.Count == 0;
+}
