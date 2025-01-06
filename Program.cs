ac4665d25d06cc7f769a8065791e88d4b47b6698
Apolos666 <quangprox121@gmail.com>
2025-01-06 15:00:17 +0700

    Bai 32 HelloInterview Solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿var s = "()(()";
+
+Console.WriteLine(LongestValidParentheses(s));
+
+int LongestValidParentheses(string s) {
+    var stack = new Stack<int>();
+    var max = 0;
+    stack.Push(-1);
+
+    for (var i = 0; i < s.Length; i++)
+    {
+        if (s[i] == '(')
+        {
+            stack.Push(i);
+        }
+        else
+        {
+            stack.Pop();
+            if (stack.Count == 0)
+            {
+                stack.Push(i);
+            }
+            else
+            {
+                max = Math.Max(max, i - stack.Peek());
+            }
+        }
+    }
+
+    return max;
+}
