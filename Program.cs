b52effda47e3bdc6db1c6e8dc251674c867029fb
Apolos666 <quangprox121@gmail.com>
2025-01-06 15:24:47 +0700

    Bai 682 My Version

--- Program.cs
+++ Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿var operations = new string[] { "5", "-2", "4", "C", "D", "9", "+", "+" };
+
+var result = CalPoints(operations);
+
+Console.WriteLine(result);
+
+int CalPoints(string[] operations)
+{
+    var stack = new Stack<int>();
+
+    foreach (var operation in operations)
+    {
+        switch (operation)
+        {
+            case "C":
+                stack.Pop();
+                break;
+            case "D":
+                stack.Push(stack.Peek() * 2);
+                break;
+            case "+":
+                var pop1 = stack.Pop();
+                var pop2 = stack.Pop();
+                var sum = pop1 + pop2;
+                stack.Push(pop2);
+                stack.Push(pop1);
+                stack.Push(sum);
+                break;
+            default:
+                stack.Push(int.Parse(operation));
+                break;
+        }
+    }
+    
+    return stack.Sum();
+}
