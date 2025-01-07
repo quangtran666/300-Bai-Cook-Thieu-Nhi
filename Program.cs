acb531588902fa2454f0a633863ba3a2bb66018f
Apolos666 <quangprox121@gmail.com>
2025-01-07 20:08:14 +0700

    Bai 739 My Solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿int[] temperatures = [30, 40, 50, 60];
+
+foreach (var temperature in DailyTemperatures(temperatures))
+{
+    Console.WriteLine(temperature);
+}
+
+int[] DailyTemperatures(int[] temperatures)
+{
+    var result = new int[temperatures.Length];
+    var monotonicStack = new Stack<int>();
+
+    for (var x = 0; x < temperatures.Length; x++)
+    {
+        while (monotonicStack.Any() && temperatures[x] > temperatures[monotonicStack.Peek()])
+        {
+            var value = monotonicStack.Pop();
+            result[value] = x - value;
+        }
+
+        monotonicStack.Push(x);
+    }
+
+    return result;
+}
