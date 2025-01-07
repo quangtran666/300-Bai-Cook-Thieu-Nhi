a6d56337d4e0e7fe32fc81c09d28eed70e9bd6aa
Apolos666 <quangprox121@gmail.com>
2025-01-07 19:48:56 +0700

    Monotonic Stack Greater My Solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿int[] nums = [2, 1, 3, 2, 4, 3];
+
+foreach (var element in NextGreaterElements(nums))
+{
+    Console.WriteLine(element);
+}
+
+int[] NextGreaterElements(int[] nums)
+{
+    var monotonicStack = new Stack<int>();
+    var result = new int[nums.Length];
+    Array.Fill(result, -1);
+
+    for (var x = 0; x < nums.Length; x++)
+    {
+        while (monotonicStack.Any() && nums[x] > nums[monotonicStack.Peek()])
+        {
+            result[monotonicStack.Pop()] = nums[x];
+        }
+        monotonicStack.Push(x);
+    }
+
+    return result;
+}
