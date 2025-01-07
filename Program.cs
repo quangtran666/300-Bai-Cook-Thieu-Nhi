7410e8dd86ba06370f75d013ec26251bc6a15fa7
Apolos666 <quangprox121@gmail.com>
2025-01-07 13:43:55 +0700

    Bai 1544 My Solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿var s = "kkdsFuqUfSDKK";
+
+Console.WriteLine(MakeGood(s));
+
+string MakeGood(string s)
+{
+    var goodStringStack = new Stack<char>();
+
+    foreach (var character in s)
+    {
+        if (goodStringStack.Any() && goodStringStack.Peek() != character && (char.ToUpper(goodStringStack.Peek()) == character || goodStringStack.Peek() == char.ToUpper(character)))
+        {
+            goodStringStack.Pop();
+        }
+        else
+        {
+            goodStringStack.Push(character);
+        }
+    }
+
+    return string.Concat(goodStringStack.Select(x => x)).Reverse().ToString();
+}
