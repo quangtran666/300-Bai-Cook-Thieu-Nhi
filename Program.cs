4d5bfe0bfec2fd4e5d72976aca20090b9e851548
Apolos666 <quangprox121@gmail.com>
2025-01-09 17:43:13 +0700

    Bai 875 Brute Force Approch Hello Interview Solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿var piles = new[] {30,11,23,4,20};
+var h = 6;
+
+var result = MinEatingSpeed(piles, h);
+Console.WriteLine(result);
+
+int MinEatingSpeed(int[] piles, int h) {
+    var speed = 1;
+    while (TimeTake(speed) > h)
+    {
+        speed++;
+    }
+
+    return speed;
+
+    int TimeTake(int speed)
+    {
+        var time = piles.Sum(pile => (pile + speed - 1) / speed);
+        return time;
+    }
+}
