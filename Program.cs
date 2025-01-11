4b116dbedb5f343304a2b5333f356e62c1d2db1f
Apolos666 <quangprox121@gmail.com>
2025-01-11 17:23:58 +0700

    Bai 153 Greg Hogg Solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿int[] nums = [3, 4, 5, 1, 2];
+
+var result = FindMin(nums);
+Console.WriteLine(result);
+
+int FindMin(int[] nums)
+{
+    var left = 0;
+    var right = nums.Length - 1;
+
+    while (left < right)
+    {
+        var mid = (left + right) / 2;
+        if (nums[mid] > nums[right])
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+
+    return nums[left];
+}
