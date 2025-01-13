12211da51e8e21c590cbfe7752389131c7f4bbcb
Apolos666 <quangprox121@gmail.com>
2025-01-13 10:06:15 +0700

    Bai 2824 Bruto Force Solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿int[] nums = [-1, 1, 2, 3, 1];
+
+const int target = 2;
+
+Console.WriteLine(CountPairs(nums, target));
+
+int CountPairs(IList<int> nums, int target) {
+    var count = 0;
+    
+    for (var x = 0; x < nums.Count; x++) {
+        for (var y = x + 1; y < nums.Count; y++)
+        {
+            if (x < y && (nums[x] + nums[y] < target))
+                count++;
+        }
+    }
+    
+    return count;
+}
