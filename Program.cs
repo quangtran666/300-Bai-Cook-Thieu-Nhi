80ab83956873edc3472ab830ba493482df88f0bd
Apolos666 <quangprox121@gmail.com>
2025-01-20 16:47:56 +0700

    Bai 1046 Greq Hogg Solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿var stones = new int[] { 2,2 };
+
+Console.WriteLine(LastStoneWeight(stones));
+
+int LastStoneWeight(int[] stones)
+{
+    var maxHeap = new PriorityQueue<int, int>();
+    foreach (var stone in stones)
+    {
+        maxHeap.Enqueue(stone, -stone);
+    }
+
+    while (maxHeap.Count > 1)
+    {
+        var y = maxHeap.Dequeue();
+        var x = maxHeap.Dequeue();
+        var result = y - x;
+        if (result != 0)
+        {
+            maxHeap.Enqueue(result, -result);
+        }
+    }
+
+    return maxHeap.Count != 0 ? maxHeap.Peek() : 0;
+}
