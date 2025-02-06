e2f64524eb9f26ddb3b3267c7f6ad67aea378c65
Apolos666 <quangprox121@gmail.com>
2025-02-06 17:45:32 +0700

    Bai 169 My solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int[] nums = [3, 2, 3];
+
+Console.Write(MajorityElement(nums));
+
+int MajorityElement(int[] nums)
+{
+    var dictionary = new Dictionary<int, int>();
+
+    foreach (var num in nums)
+    {
+        dictionary[num] = dictionary.GetValueOrDefault(num, 1) + 1;
+    }
+
+    return dictionary
+        .First(x => x.Value == dictionary.Values.Max())
+        .Key;
+}
