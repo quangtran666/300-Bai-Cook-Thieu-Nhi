d2111861d9e8716d9eb20296a42374d2ef952c30
Apolos666 <quangprox121@gmail.com>
2025-02-06 09:05:25 +0700

    Bai 1929 My solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿var nums = new int[] { 1, 2, 1 };
+
+var result = GetConcatenation(nums);
+Console.WriteLine(string.Join(", ", result));
+
+int[] GetConcatenation(int[] nums)
+{
+    var result = new int[nums.Length * 2];
+
+    for (var i = 0; i < nums.Length; i++)
+    {
+        result[i] = nums[i];
+        result[i + nums.Length] = nums[i];
+    }
+    
+    return result;
+}
