aa27c53f3b3ec4adb3ef2d86dec6d12d8becfa8c
Apolos666 <quangprox121@gmail.com>
2025-02-06 17:54:54 +0700

    Bai 268 My solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿int MissingNumber(int[] nums)
+{
+    var hashset = new HashSet<int>(nums);
+
+    for (var i = 0; i <= nums.Length; i++)
+    {
+        if (!hashset.Contains(i))
+            return i;
+    }
+
+    return 0;
+}
