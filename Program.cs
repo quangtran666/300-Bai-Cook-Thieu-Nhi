cf498d6731e2e603b07e5e9a87e4a2e5d588cdb1
Apolos666 <quangprox121@gmail.com>
2025-02-06 17:59:35 +0700

    Bai 349 My Solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿int[] nums1 = [1, 2, 2, 1];
+int[] nums2 = [2, 2];
+
+Console.Write(string.Join(" ", Intersection(nums1, nums2)));
+
+int[] Intersection(int[] nums1, int[] nums2)
+{
+    var hashset1 = new HashSet<int>(nums1);
+    var hashset2 = new HashSet<int>(nums2);
+
+    return hashset1.Intersect(hashset2).ToArray();
+}
