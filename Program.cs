7f99b4fd1609d953a7ba78e60a2e56361c79eb2c
Apolos666 <quangprox121@gmail.com>
2025-02-08 16:30:39 +0700

    Bai 88 Leetcode solution

--- Program.cs
+++ Program.cs
@@ -1,17 +1,30 @@
-﻿var nums1 = new int[] { 4,5,6,0,0,0 };
+﻿var nums1 = new int[] { 0 };
 
-var nums2 = new int[] { 1, 2, 3 };
+var nums2 = new int[] { 1 };
 
-Merge(nums1, 3, nums2, 3);
+Merge(nums1, 0, nums2, 1);
 
 Console.WriteLine(string.Join(", ", nums1));
 
 void Merge(int[] nums1, int m, int[] nums2, int n)
 {
-    for (int i = 0; i < n; i++)
+    var i = m - 1;
+    var j = n - 1;
+    var k = m + n - 1;
+
+    while (j >= 0)
     {
-        nums1[m + i] = nums2[i];
+        if (i >= 0 && nums1[i] > nums2[j])
+        {
+            nums1[k] = nums1[j];
+            i--;
+        }
+        else
+        {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        
+        k--;
     }
-    
-    Array.Sort(nums1);
 }
--- obj/Debug/net8.0/Console.AssemblyInfo.cs
+++ obj/Debug/net8.0/Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf498d6731e2e603b07e5e9a87e4a2e5d588cdb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7c0f67f8a19a8e755a4fb6168d95f53a39d3168")]
 [assembly: System.Reflection.AssemblyProductAttribute("Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
